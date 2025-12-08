06df6639fc5346c04af873b5b48387cdff8ef4bb
DAVID GAMBOA VILLALOBOS <dgamboa@msj.go.cr>
2025-12-08 14:14:17 -0600

    Add ProtectedRoute to secure application

--- src/App.tsx
+++ src/App.tsx
@@ -41,64 +41,65 @@ import IngresoActivos from './pages/Activos/IngresoActivos';
 import AsignacionActivos from './pages/Activos/AsignacionActivos';
 import AccesoriosActivos from './pages/Activos/AccesoriosActivos';
 import { ThemeProvider } from './context/ThemeContext';
+import { ProtectedRoute } from './components/ProtectedRoute';
 
 function App() {
     return (
         <ThemeProvider>
             <Routes>
                 <Route path="/login" element={<Login />} />
-                <Route path="/*" element={
-                    <Layout>
-                        <Routes>
-                            <Route path="/" element={<Dashboard />} />
-                            <Route path="/articulos" element={<Articulos />} />
-                            <Route path="/articulos/consultar-inventario" element={<ConsultarInventario />} />
-                            <Route path="/articulos/escaner-qr" element={<EscanerQR />} />
-                            <Route path="/articulos/gestion-imagenes" element={<GestionImagenes />} />
-                            <Route path="/articulos/ingresar-articulo" element={<IngresarArticulo />} />
-                            <Route path="/articulos/generar-etiqueta" element={<GenerarEtiqueta />} />
-                            <Route path="/articulos/historial-articulo" element={<HistorialArticulo />} />
-                            <Route path="/articulos/consultar-salidas" element={<ConsultarSalidas />} />
-                            <Route path="/articulos/devoluciones" element={<Devoluciones />} />
-                            <Route path="/articulos/kardex-diario" element={<KardexDiario />} />
+                <Route element={<ProtectedRoute />}>
+                    <Route path="/*" element={
+                        <Layout>
+                            <Routes>
+                                <Route path="/" element={<Dashboard />} />
+                                <Route path="/articulos" element={<Articulos />} />
+                                <Route path="/articulos/consultar-inventario" element={<ConsultarInventario />} />
+                                <Route path="/articulos/escaner-qr" element={<EscanerQR />} />
+                                <Route path="/articulos/gestion-imagenes" element={<GestionImagenes />} />
+                                <Route path="/articulos/ingresar-articulo" element={<IngresarArticulo />} />
+                                <Route path="/articulos/generar-etiqueta" element={<GenerarEtiqueta />} />
+                                <Route path="/articulos/historial-articulo" element={<HistorialArticulo />} />
+                                <Route path="/articulos/consultar-salidas" element={<ConsultarSalidas />} />
+                                <Route path="/articulos/devoluciones" element={<Devoluciones />} />
+                                <Route path="/articulos/kardex-diario" element={<KardexDiario />} />
 
-                            {/* Cliente Interno */}
-                            <Route path="/cliente-interno" element={<ClienteInterno />} />
-                            <Route path="/cliente-interno/ingresar" element={<IngresarSolicitud />} />
-                            <Route path="/cliente-interno/seguimiento" element={<SeguimientoSolicitud />} />
-                            <Route path="/cliente-interno/realizar-salidas" element={<TablaSolicitudesSalida />} />
-                            <Route path="/cliente-interno/realizar-salidas/formulario" element={<RealizarSalida />} />
-                            <Route path="/cliente-interno/consultar-estado" element={<ConsultarEstadoSolicitud />} />
+                                {/* Cliente Interno */}
+                                <Route path="/cliente-interno" element={<ClienteInterno />} />
+                                <Route path="/cliente-interno/ingresar" element={<IngresarSolicitud />} />
+                                <Route path="/cliente-interno/seguimiento" element={<SeguimientoSolicitud />} />
+                                <Route path="/cliente-interno/realizar-salidas" element={<TablaSolicitudesSalida />} />
+                                <Route path="/cliente-interno/realizar-salidas/formulario" element={<RealizarSalida />} />
+                                <Route path="/cliente-interno/consultar-estado" element={<ConsultarEstadoSolicitud />} />
 
-                            {/* Cliente Externo */}
-                            <Route path="/cliente-externo" element={<ClienteExterno />} />
-                            <Route path="/cliente-externo/ingresar" element={<IngresarSolicitudExterno />} />
-                            <Route path="/cliente-externo/seguimiento" element={<SeguimientoSolicitudExterno />} />
-                            <Route path="/cliente-externo/registro-salida" element={<RegistroSalidaExterno />} />
-                            <Route path="/cliente-externo/realizar" element={<SolicitudesExternasTable />} />
-                            <Route path="/otras-solicitudes" element={<OtrasSolicitudes />} />
-                            <Route path="/otras-solicitudes/equipos-activos" element={<EquiposActivos />} />
-                            <Route path="/otras-solicitudes/herramientas" element={<Herramientas />} />
-                            <Route path="/otras-solicitudes/prestamo" element={<Prestamo />} />
-                            <Route path="/otras-solicitudes/articulos-oficina" element={<ArticulosOficina />} />
-                            <Route path="/otras-solicitudes/sin-asignacion" element={<SinAsignacion />} />
-                            <Route path="/otras-solicitudes/taller-ebanisteria" element={<TallerEbanisteria />} />
-                            <Route path="/otras-solicitudes/vestimenta" element={<Vestimenta />} />
-                            <Route path="/otras-solicitudes/limpieza-aseo" element={<LimpiezaAseo />} />
-                            <Route path="/gestion-interna" element={<GestionInterna />} />
-                            <Route path="/gestion-interna/colaboradores" element={<InformeColaboradores />} />
+                                {/* Cliente Externo */}
+                                <Route path="/cliente-externo" element={<ClienteExterno />} />
+                                <Route path="/cliente-externo/ingresar" element={<IngresarSolicitudExterno />} />
+                                <Route path="/cliente-externo/seguimiento" element={<SeguimientoSolicitudExterno />} />
+                                <Route path="/cliente-externo/registro-salida" element={<RegistroSalidaExterno />} />
+                                <Route path="/cliente-externo/realizar" element={<SolicitudesExternasTable />} />
+                                <Route path="/otras-solicitudes" element={<OtrasSolicitudes />} />
+                                <Route path="/otras-solicitudes/equipos-activos" element={<EquiposActivos />} />
+                                <Route path="/otras-solicitudes/herramientas" element={<Herramientas />} />
+                                <Route path="/otras-solicitudes/prestamo" element={<Prestamo />} />
+                                <Route path="/otras-solicitudes/articulos-oficina" element={<ArticulosOficina />} />
+                                <Route path="/otras-solicitudes/sin-asignacion" element={<SinAsignacion />} />
+                                <Route path="/otras-solicitudes/taller-ebanisteria" element={<TallerEbanisteria />} />
+                                <Route path="/otras-solicitudes/vestimenta" element={<Vestimenta />} />
+                                <Route path="/otras-solicitudes/limpieza-aseo" element={<LimpiezaAseo />} />
+                                <Route path="/gestion-interna" element={<GestionInterna />} />
+                                <Route path="/gestion-interna/colaboradores" element={<InformeColaboradores />} />
 
-                            {/* Gestión de Activos */}
-                            <Route path="/activos" element={<Activos />} />
-                            <Route path="/activos/inventario" element={<InventarioActivos />} />
-                            <Route path="/activos/ingreso" element={<IngresoActivos />} />
-                            <Route path="/activos/asignacion" element={<AsignacionActivos />} />
-                            <Route path="/activos/accesorios" element={<AccesoriosActivos />} />
-
-
-                        </Routes>
-                    </Layout>
-                } />
+                                {/* Gestión de Activos */}
+                                <Route path="/activos" element={<Activos />} />
+                                <Route path="/activos/inventario" element={<InventarioActivos />} />
+                                <Route path="/activos/ingreso" element={<IngresoActivos />} />
+                                <Route path="/activos/asignacion" element={<AsignacionActivos />} />
+                                <Route path="/activos/accesorios" element={<AccesoriosActivos />} />
+                            </Routes>
+                        </Layout>
+                    } />
+                </Route>
             </Routes>
         </ThemeProvider>
     );
