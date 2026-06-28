541bcf297e65182af1feb250ebfc29adb333e315
DAVID GAMBOA VILLALOBOS <dgamboa@msj.go.cr>
2026-06-27 22:22:56 -0600

    style: cambiar tile layer del mapa a modo claro estandar OpenStreetMap

--- src/pages/proyectos-obra/ProyectosObraMapa.tsx
+++ src/pages/proyectos-obra/ProyectosObraMapa.tsx
@@ -73,8 +73,8 @@ export default function ProyectosObraMapa() {
               style={{ height: '100%', width: '100%', backgroundColor: '#18181b' }}
             >
               <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-                url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
 
               {proyectos.map((p) => {
